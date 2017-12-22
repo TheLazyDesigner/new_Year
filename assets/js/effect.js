857ece2e23fbca21acc0b46b5a493dafbb8f00f1
TheLazyDesigner <mathieu.croset@gmail.com>
2017-12-22 13:49:55 +0100

    Add files via upload

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -1,12 +1,17 @@
 $(window).on( 'load', function(){
-  $('body').addClass('page-loading-step1');
   setTimeout(function () {
-    $('#layer-gif-1 img').attr('src','assets/images/season_winter_icone-coeur.gif');
-  }, 2000 );
+    $('body').addClass('page-loading-step1');
+  }, 300 );
   setTimeout(function () {
-    $('#layer-gif-2 img').attr('src','assets/images/season_winter_icone-sapin.gif');
-  }, 3000 );
+    $('body').addClass('page-loading-step2');
+  }, 600 );
   setTimeout(function () {
-    $('#layer-gif-3 img').attr('src','assets/images/season_winter_icone-luge.gif');
-  }, 6000 );
+    $('body').addClass('page-loading-step3');
+  }, 900 );
+  setTimeout(function () {
+    $('body').addClass('page-loading-step4');
+  }, 1200 );
+  setTimeout(function () {
+    $('body').addClass('page-loading-step5');
+  }, 1500 );
 });
